d6b9d90366144339386fc979fa0914d1a3d4973f
eardleydev <ryan.eardley@gmail.com>
2023-01-21 12:02:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn Reactt man
           </a>
         </header>
       </div>
